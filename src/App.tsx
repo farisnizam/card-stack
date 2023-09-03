41c508efe077510bfd430f0f6d7c4e130313191c
farisnizam <farismtq@gmail.com>
2023-09-03 22:14:00 +0800

    add DeckProps, set condition for drag direction

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import Deck from './Components/Deck'
 import styles from './styles.module.css'
 
 export default function App() {
+  const cardArray = [
+    'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg',
+  ]
+
   return (
     <div className={styles.container}>
-      <Deck />
+      <Deck cardList={cardArray} verticalDrag={true} />
     </div>
   )
 }
--- src/Components/Deck.tsx
+++ src/Components/Deck.tsx
@@ -4,7 +4,13 @@ import { useDrag } from 'react-use-gesture'
 
 import styles from '../styles.module.css'
 
-const cards = [
+interface DeckProps {
+  cardList?: string[]
+  verticalDrag?: boolean
+  horizontalDrag?: boolean
+}
+
+const defaultCards = [
   'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
@@ -26,15 +32,16 @@ const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 const trans = (r: number, s: number) =>
   `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
-const Deck = () => {
+const Deck = ({ cardList = defaultCards, verticalDrag = false, horizontalDrag = true }: DeckProps) => {
   const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-  const [props, api] = useSprings(cards.length, i => ({
+  const [props, api] = useSprings(cardList.length, i => ({
     ...to(i),
     from: from(i),
   })) // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
 
   const bind = useDrag(({ args: [index], down, movement: [mx, my], direction: [xDir, yDir], velocity }) => {
+    console.log('test velocity', velocity)
     const trigger = velocity > 2.2 // If you flick hard enough it should trigger the card to fly out
     const dirX = xDir < 0 ? -1 : 1 // Direction should either point left or right
     const dirY = yDir < 0 ? -1 : 1 // Direction should either point up or down
@@ -44,8 +51,9 @@ const Deck = () => {
     api.start(i => {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dirX : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-      const y = isGone ? (200 + window.innerHeight) * dirY : down ? my : 0 // When a card is gone it flys out up or down, otherwise goes back to zero
+      const x = horizontalDrag ? (isGone ? (200 + window.innerWidth) * dirX : down ? mx : 0) : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const y = verticalDrag ? (isGone ? (200 + window.innerHeight) * dirY : down ? my : 0) : 0 // When a card is gone it flys out up or down, otherwise goes back to zero
+      //   const y = 0 // When a card is gone it flys out up or down, otherwise goes back to zero
       const rot = mx / 100 + my / 100 + (isGone ? (dirX * 10 + dirY * 10) * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
       const scale = down ? 1.1 : 1 // Active cards lift up a bit
       return {
@@ -58,7 +66,7 @@ const Deck = () => {
       }
     })
 
-    if (!down && gone.size === cards.length) {
+    if (!down && gone.size === cardList.length) {
       setTimeout(() => {
         gone.clear()
         api.start(i => to(i))
@@ -75,7 +83,7 @@ const Deck = () => {
             {...bind(i)}
             style={{
               transform: interpolate([rot, scale], trans),
-              backgroundImage: `url(${cards[i]})`,
+              backgroundImage: `url(${cardList[i]})`,
             }}
           />
         </animated.div>
